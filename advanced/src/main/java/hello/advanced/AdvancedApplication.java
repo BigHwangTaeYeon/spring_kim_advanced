073d9e8cc1d967533cc05746e5a1d6f9e65e6ad0
BigHwang <gxd0214@gmail.com>
2023-11-19 22:12:15 +0900

    로그 추적기 V1

--- advanced/src/main/java/hello/advanced/AdvancedApplication.java
+++ advanced/src/main/java/hello/advanced/AdvancedApplication.java
@@ -3,11 +3,17 @@ package hello.advanced;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+
 @SpringBootApplication
 public class AdvancedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdvancedApplication.class, args);
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.end(status);
 	}
 
 }
--- advanced/src/main/java/hello/advanced/app/v0/OrderServiceV0.java
+++ advanced/src/main/java/hello/advanced/app/v0/OrderServiceV0.java
@@ -2,13 +2,14 @@ package hello.advanced.app.v0;
 
 import org.springframework.stereotype.Service;
 
+import hello.advanced.app.v1.OrderRepositoryV1;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class OrderServiceV0 {
 
-    private final OrderRepositoryV0 orderRepository;
+    private final OrderRepositoryV1 orderRepository;
 
     public void orderItem(String itemId) {
         orderRepository.save(itemId);
